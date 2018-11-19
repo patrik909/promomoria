37b0ab050638a0abd418748cd1ea11273f6f2250
patrik909 <patrik.eriksson@medieinstitutet.se>
2018-11-19 13:39:38 +0100

    added react app

---
+++ promo/src/Promomoria.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="Promomoria">
+        <h1> This is Promomoria </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
