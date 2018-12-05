f307c47f1ef28de97d4a2722f45d2889cb5d67df
patrik909 <patrik.eriksson@medieinstitutet.se>
2018-12-05 17:02:47 +0100

    Artwork upload is working correctly now

--- promo/src/Components/AddReleaseArtwork.js
+++ promo/src/Components/AddReleaseArtwork.js
@@ -7,15 +7,38 @@ class AddReleaseArtwork extends Component {
     state = {
         imageName: '',
         imageUrl: '',
-        loaded: 0
+        loaded: 0,
+        message: ''
     }
 
-    addArtwork = event => {
-
+    checkArtwork = event => {
         let image = event.target.files[0];
+
+        if (event.target.files && event.target.files[0]) {
+            var reader = new FileReader();
+            reader.onload = (e) => {
+              var img = new Image;
+              img.onload = () => {
+                  if ( img.width > 1399 && img.height > 1399 ) {
+                    this.addArtwork(image);
+                    this.setState({ message: '' })
+                  } else {
+                    this.setState({ message: 'Artwork is to small!' })
+                  }
+                console.log(img.width);
+                console.log(img.height);
+              };
+              img.src = reader.result;
+            };
+            reader.readAsDataURL(event.target.files[0]);
+          }
+        
+    }
+
+    addArtwork = image => {
         const data = new FormData();
         data.append('artwork', image, image.name);
-            
+
         axios.post('api/upload_artwork', data, {
             onUploadProgress: ProgressEvent => {
                 this.setState({
@@ -47,26 +70,42 @@ class AddReleaseArtwork extends Component {
   
         return (
             <div className="ArtworkUploader">
-                {this.state.imageUrl ? (
-                    <div className="ArtworkHolder">
-                        <img src={this.state.imageUrl ? 'api/' + this.state.imageUrl : null} alt="Release Artwork" /> 
-                        <Button 
-                            innerText={'Delete'}
-                            onClick={this.removeArtwork}
-                        />
-                    </div>
-                ) : (
-                    <div className="center column">
-                        <label htmlFor="uploadArtworkInput">Choose a file</label>
-                        <div id="artworkProcessBar"><div className="fileProcessed" style={{width : Math.round(this.state.loaded,2) + '%'}}></div> </div>
-                    </div>
-                )}
-                <input 
-                    type="file" 
-                    className="hideInputFile" 
-                    id="uploadArtworkInput"
-                    onChange={this.addArtwork} 
-                />
+                <p className="helper">Image have to be 1400x1400px or larger</p>
+                <div className="ArtworkHolder">
+                    {
+                        !this.state.imageUrl ? (
+                            <div>
+                                <label htmlFor="uploadArtworkInput" className="ArtworkFileInput">
+                                    <div className="ArtworkPlaceholderIcon"><div className="CenterHole"></div></div>
+                                    <p>Choose artwork file</p>
+                                    {
+                                        this.state.message ? (
+                                            <p className="ToSmallImage">{this.state.message}</p>
+                                        ) : ( null )
+                                    }
+                                </label>
+                                <input 
+                                    type="file" 
+                                    className="hideInputFile" 
+                                    id="uploadArtworkInput"
+                                    onChange={this.checkArtwork} 
+                                />
+                                <div className="ArtworkProcessBar">
+                                    <div className="FileProcessed" style={{ width : Math.round(this.state.loaded, 2) + '%' }}></div> 
+                                </div>
+                            </div>
+                        ) : (
+                            <div className="ArtworkImage" style={{width: '100%', height: '100%', overflow:'hidden', position:'relative'}}>
+                                <img src={this.state.imageUrl ? 'api/' + this.state.imageUrl : null} alt="Release Artwork" style={{width: '100%', position: ' absolute'}}/>
+                                <Button 
+                                    className={'DeleteArtwork'}
+                                    innerText={'X'}
+                                    onClick={this.removeArtwork}
+                                /> 
+                            </div>
+                        )
+                    }
+                </div>
             </div>
         );
     }
--- promo/src/Components/AddReleaseInfo.js
+++ promo/src/Components/AddReleaseInfo.js
@@ -1,58 +1,57 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Inputfield from '../Components/Parts/Inputfield.js';
 
-class AddReleaseInfo extends Component {
+function AddReleaseInfo(props) {
 
-    render() {
-        return (
-            <div className="AddReleaseInfo">
-                <p>Fields with * is required</p>
+    return (
+        <div className="AddReleaseInfo">
+            <p className="helper">Fields with * is required</p>
+            <Inputfield 
+                className={'full-width'}
+                placeholder={'Artist *'} 
+                onChange={props.handleArtist}
+            />
+            <Inputfield 
+                className={'full-width'}
+                placeholder={'Title *'} 
+                onChange={props.handleTitle}
+            />
+            <div className={'flex'}>
                 <Inputfield 
-                    className={'full-width'}
-                    placeholder={'Artist *'} 
-                    onChange={this.props.handleArtist}
+                    className={'half-width'}
+                    placeholder={'Cat nr *'} 
+                    onChange={props.handleCatNr}
                 />
                 <Inputfield 
-                    className={'full-width'}
-                    placeholder={'Title *'} 
-                    onChange={this.props.handleTitle}
+                    className={'half-width'}
+                    placeholder={'Password *'} 
+                    onChange={props.handlePassword}
                 />
-                <div className={'flex'}>
-                    <Inputfield 
-                        className={'half-width'}
-                        placeholder={'Cat nr *'} 
-                        onChange={this.props.handleCatNr}
-                    />
-                    <Inputfield 
-                        className={'half-width'}
-                        placeholder={'Password *'} 
-                        onChange={this.props.handlePassword}
-                    />
-                </div>
-                <div className={'flex'}>
-                    <Inputfield 
-                        className={'half-width'}
-                        placeholder={'Release date'} 
-                        onChange={this.props.handleReleaseDate}
-                    />
-                    <div className="EnableRating half-width flex">
-                        <p>Enable rating:</p>
-                        <label htmlFor="enable-rating" style={{background : this.props.ratingEnabled ? ( 'rgb(213, 32, 32)' ) : ( null )}}></label>
-                        <input type="checkbox" className="hide" id="enable-rating" value={1} onClick={this.props.handleRating} />
-                    </div>
-                </div>
-              
-                <textarea
-                    className="full-width"
-                    placeholder="Info text*"
-                    rows="12"
-                    maxLength="500"
-                    onChange={this.props.handleInfoText}
-                ></textarea>
-                <p className="UsedCharacters">Used characters: {this.props.infoTextLength} / 500</p>
             </div>
-        );
-    }
+            <div className={'flex'}>
+                <Inputfield 
+                    className={'half-width'}
+                    placeholder={'Release date'} 
+                    onChange={props.handleReleaseDate}
+                />
+                <div className="EnableRating half-width flex">
+                    <p>Enable rating:</p>
+                    <label htmlFor="enable-rating" style={{background : props.ratingEnabled ? ( 'rgb(213, 32, 32)' ) : ( null )}}></label>
+                    <input type="checkbox" className="hide" id="enable-rating" value={1} onClick={props.handleRating} />
+                </div>
+            </div>
+            
+            <textarea
+                className="full-width"
+                placeholder="Info text*"
+                rows="12"
+                maxLength="500"
+                onChange={props.handleInfoText}
+            ></textarea>
+            <p className="UsedCharacters">Used characters: {props.infoTextLength} / 500</p>
+        </div>
+    );
+    
 }
 
 export default AddReleaseInfo;
